bd4c19233cdabf10dbd42f17b175ede24b86b156
aguslgc <123350992+aguslgc@users.noreply.github.com>
2023-01-23 02:52:50 -0300

    Add files via upload

---
+++ examen/script.js
@@ -0,0 +1,7 @@
+const jsBar = document.getElementById("js-bar");
+const cssBar = document.getElementById("css-bar");
+const htmlBar = document.getElementById("html-bar");
+
+jsBar.style.width = "65%";
+cssBar.style.width = "70%";
+htmlBar.style.width = "95%";
